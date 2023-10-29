f9933a57697e1813afc3e6da32eb9bdffa33c050
mexveritas <147470609+mexveritas@users.noreply.github.com>
2023-10-29 17:31:43 +0900

    항력 계수 음수로 수정

--- sketch.js
+++ sketch.js
@@ -7,7 +7,7 @@ function setup() {
   createCanvas(640, 240);
   reset();
   // Create liquid object
-  liquid = new Liquid(0, height / 2, width, height / 2, 0.25);
+  liquid = new Liquid(0, height / 2, width, height / 2, -0.05);
 }
 
 function draw() {
