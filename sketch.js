bddd768a96c7ce29646caf265aef518472ba701f
mexveritas <147470609+mexveritas@users.noreply.github.com>
2023-10-29 17:05:17 +0900

    p5.js 파일 추가

---
+++ sketch.js
@@ -0,0 +1,49 @@
+let movers = [];
+
+// Liquid
+let liquid;
+
+function setup() {
+  createCanvas(640, 240);
+  reset();
+  // Create liquid object
+  liquid = new Liquid(0, height / 2, width, height / 2, 0.1);
+}
+
+function draw() {
+  background(255);
+
+  // Draw liquid
+  liquid.show();
+
+  for (let i = 0; i < movers.length; i++) {
+    // Is the Mover in the liquid?
+    if (liquid.contains(movers[i])) {
+      // Calculate drag force
+      let dragForce = liquid.calculateDrag(movers[i]);
+      // Apply drag force to Mover
+      movers[i].applyForce(dragForce);
+    }
+
+    // Gravity is scaled by mass here!
+    let gravity = createVector(0, 0.1 * movers[i].mass);
+    // Apply gravity
+    movers[i].applyForce(gravity);
+
+    // Update and display
+    movers[i].update();
+    movers[i].show();
+    movers[i].checkEdges();
+  }
+}
+
+function mousePressed() {
+  reset();
+}
+
+// Restart all the Mover objects randomly
+function reset() {
+  for (let i = 0; i < 9; i++) {
+    movers[i] = new Mover(40 + i * 70, 0, random(0.5, 3));
+  }
+}
